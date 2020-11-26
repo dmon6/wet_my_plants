61a6c0d2ed918421db8f8314d951010781d8c02e
Dennis Monton <dmonton66@gmail.com>
2020-11-25 19:53:59 -0500

    Changed port in the execute script

--- relay_control.py
+++ relay_control.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 class SocketControl():
     '''Simple Class to send commands to relay for waterpump'''
@@ -6,34 +7,41 @@ class SocketControl():
     TURN_ON_RELAY = '\xfe\x00'
     TURN_OFF_RELAY = '\xfe\x08'
 
-    def __init__(self, device_ip, device_port):
-        self.device_ip = device_ip
-        self.device_port = device_port
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
         self.connected = False
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.settimeout(1)
         
         try:
-            self.sock.connect((self.device_ip, self.device_port))
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((self.ip, self.port))
             self.connected = True
         except Exception:
-            print('Failed to connect to {}'.format(self.device_ip))
+            print('Failed to connect to {}'.format(self.ip))
 
     def send_cmd(self, cmd):
+        '''Sends command through the socket'''
+        response = None
         if self.connected:
             self.sock.send(cmd + '\r')
             response = self.sock.recv(4096)
         return response      
 
     def power_on(self):
+        '''Turns on the relay'''
         return self.send_cmd(self.TURN_ON_RELAY)
 
     def power_off(self):
+        '''Turns off the relay'''
         return self.send_cmd(self.TURN_OFF_RELAY)      
         
 if __name__ == '__main__':
     # Device Settings
-    device_ip = '192.168.1.20'
-    device_port = 10001
+    ip = '192.168.1.20'
+    port = 2101
 
-    
+    test_dev = SocketControl(ip, port)
+    test_dev.power_on()
+    time.sleep(3)
+    test_dev.power_off()
+    del test_dev
