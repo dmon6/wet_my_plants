4ccc164e2b7b62a915aac82ba249177720200d5e
Dennis Monton <dmonton66@gmail.com>
2020-12-05 16:03:35 -0500

    Flask app that handles template logic

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for
+from markupsafe import escape
+from services.serial_control import SerialControl
+import os
+
+app = Flask(__name__)
+
+posts = [{
+                'filename': 'media/baby_yoda.png',
+                'description': 'Baby Yoda'
+            },
+            {
+                'filename': 'media/boba_fett.jpg',
+                'description': 'Boba Fett'
+            }]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/pictures', methods=['GET', 'POST']) 
+def pictures():
+    return render_template('pictures.html', title='Pictures', posts=posts)
+
+if __name__ == '__main__':
+    app.run(debug=True)
